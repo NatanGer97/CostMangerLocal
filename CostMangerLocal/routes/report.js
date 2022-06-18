6e6e04a68e26658b705d86d72f854d85c59dea72
NatanGer97 <42692104+NatanGer97@users.noreply.github.com>
2022-06-18 22:32:40 +0300

    changed retriving reports via promise

--- CostMangerLocal/routes/report.js
+++ CostMangerLocal/routes/report.js
@@ -26,65 +26,54 @@ router.get('/:userId/newReport', async function (req, res) {
     const year = yearAndMonth[0];
     const month = yearAndMonth[1];
 
-    
-    
-    Report.find({userId: req.params.userId, date_created : fullDate}, function(err, report){
-        if (err)
+    try{
+        const report = await Report.find({userId: req.params.userId, date_created : fullDate});
+        
+        // case : creating new report
+        if  (report.length === 0)
         {
-            res.send(err);
-        }
-        console.log(report);
-        res.json(report);
-    }); 
-
-
-    // const costsArray = costs.filter(cost => 
-    //     cost.date.split("-")[1] === month && cost.date.split("-")[0] === year 
-    // );
-    
-    // let sum = 0;
-
-    // costsArray.forEach((cost) => sum += cost.sum);
-    // console.log(sum);
-
-    
-    // const generateNewReport = new Report({
-    //     userId : req.params.userId,
-    //     costs: costsArray,
-    //     totalSum : sum,
-    //     date_created : new Date(fullDate).toISOString().split('-').slice(0,2).join('-'),
-    // });
-    
-    // await generateNewReport.save();
-
-    // splitting date in  wed, 14 Jun 2022 07:00:00 GMT form to have only Jun, 2022 via Regex
-    // const dateInArrFormat = reportDate.split(/\W+/gm).slice(2, 4)
-    // const reportTitle = (`Report for: ${dateInArrFormat[0]}, ${dateInArrFormat[1]}`);
-
-    // const user = await User.findById(req.params.id).populate('costs');
-   /*  const costs = await Cost.find({ userId: req.params.userId });
-    const fittingCosts = costs.filter(
-        cost =>
-            (cost.date.split('-')[1] === req.query.date.split('-')[1] )
-            && (( cost.date.split('-')[0] === req.query.date.split('-')[0] ))
+            const costsArray = costs.filter(cost => 
+                cost.date.split("-")[1] === month && cost.date.split("-")[0] === year 
+            );
             
-        );
+            let sum = 0;
+        
+            costsArray.forEach((cost) => sum += cost.sum);
+            console.log(sum);
+        
+            
+            const generateNewReport = new Report({
+                userId : req.params.userId,
+                costs: costsArray,
+                totalSum : sum,
+                date_created : new Date(fullDate).toISOString().split('-').slice(0,2).join('-'),
+            });
+            
+            await generateNewReport.save(); 
+            
+            // splitting date in  wed, 14 Jun 2022 07:00:00 GMT form to have only Jun, 2022 via Regex
+            const dateInArrFormat = reportDate.split(/\W+/gm).slice(2, 4)
+            const reportTitle = (`Report for: ${dateInArrFormat[0]}, ${dateInArrFormat[1]}`);
+            
+            res.render('report/newReport', 
+            { 'reportTitle': reportTitle, 'costs': costsArray, 'totalSum' : sum});
+        
+        
+        
+        }
+        // checking existing report
+        else
+        {            
+            res.send("already exist");
+       
+        }
 
-    let totalSumInThisMonth = 0;
 
-    fittingCosts.forEach(cost => {
-        totalSumInThisMonth += cost.sum;
-    });
-    
-    console.log(totalSumInThisMonth); */
+    }catch(err)
+    {
+        res.send(err);
+    }
 
-    /* for self testing 
-    // console.log(reportDate.split(/\W+/gm).slice(2,4));    
-    */
-
-
-    // res.render('report/newReport', 
-    // { 'reportTitle': reportTitle, 'costs': costsArray, 'totalSum' : sum});
 
 });
 
